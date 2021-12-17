435e73f9438041f315b2b57e941c91e63912f98d
noahfarinas <94990436+noahfarinas@users.noreply.github.com>
2021-12-16 21:10:15 -0800

    first commit. predeploy for github

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Top Rad Power Bikes</h1>
+      <div className="runner">
+          <img src="./images/runnerplus.jpeg"/>
+        <span><h2>Rad Runner Plus</h2></span>
+        <h3>4.5 stars <a className="reviews" href="https://www.google.com/search?q=google+rad+runner+plus+reviewsz&oq=google+rad+runner+plus+reviewsz&aqs=chrome..69i57j69i64.4319j0j7&sourceid=chrome&ie=UTF-8">909 reviews</a> </h3>
+        <h3>$1,799</h3>
+          <h4>Top Features</h4>
+          <ul>
+            <li> <b>Motor:</b> 750W Geared Hub</li>
+            <li>Geared hub motor</li>
+            <li>20" x 3.3" tires</li>
+          </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
